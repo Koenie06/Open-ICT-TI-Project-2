c45e824e31183e6ea5d3d924fa6ae8d7e278adc2
Elnicio Bernabela <elniciob@outlook.com>
2026-09-21 10:51:23 +0200

    Pakt het gemiddelde, tussen de min en max

--- geluidsmonitor/Slimme geluidsmonitor_finn/src/main.cpp
+++ geluidsmonitor/Slimme geluidsmonitor_finn/src/main.cpp
@@ -3,8 +3,10 @@
 int overlast = 0;
 int kalmte = 0;
 
+const int samples = 300;
+const int deltaThreshold = 50; // pas dit aan op basis van wat je in Serial Monitor ziet
+
 void setup() {
-  // put your setup code here, to run once:
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
@@ -13,77 +15,62 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int analogValue = analogRead(0); // Measure the audio sensor input
-  long sum = 0;
-  const int samples = 200;
+  int mn = 1024;
+  int mx = 0;
 
-  // Find average (DC level)
-  for (int i = 0; i < samples; i++) {
-    sum += analogRead(0);
+  for (int i = 0; i < samples; ++i) {
+    int val = analogRead(0);
+    mn = min(mn, val);
+    mx = max(mx, val);
   }
 
-  float average = sum / (float)samples;
+  int delta = mx - mn;
 
-  // Calculate signal amplitude
-  long sumSquares = 0;
+  Serial.print("Min=");
+  Serial.print(mn);
+  Serial.print(" Max=");
+  Serial.print(mx);
+  Serial.print(" Delta=");
+  Serial.println(delta);
 
-  for (int i = 0; i < samples; i++) {
-    float x = analogRead(0) - average;
-    sumSquares += x * x;
-  }
-
-  float rms = sqrt(sumSquares / (float)samples);
-
-  Serial.println(rms);
-
-  if ((rms > 2.59) && (kalmte < 1) && (overlast < 12)) {
+  // DE ONTBREKENDE SCHAKEL: delta daadwerkelijk gebruiken om overlast bij te werken
+  if ((delta > deltaThreshold) && (kalmte < 1) && (overlast < 12)) {
     overlast = overlast + 1;
   }
-  else if ((rms < 2.50) && (overlast > 0)) {
+  else if ((delta < deltaThreshold / 2) && (overlast > 0)) {
     overlast = overlast - 1;
   }
 
-if ((overlast > 9) && (kalmte < 0)) {
+  if ((overlast > 9) && (kalmte < 0)) {
     digitalWrite(2, LOW);
     digitalWrite(3, LOW);
     digitalWrite(4, HIGH);
     digitalWrite(5, HIGH);
-    delay(500);
     digitalWrite(5, LOW);
   }
-else if (kalmte < 1) {
-  digitalWrite (2, LOW);
-  digitalWrite(3, HIGH);
-  digitalWrite(4, LOW);
-  };
+  else if (kalmte < 1) {
+    digitalWrite(2, LOW);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+  }
 
-if ((rms < 0.1) && (overlast == 0)) {
+  if (overlast == 0) {
     digitalWrite(2, HIGH);
     digitalWrite(3, LOW);
     digitalWrite(4, LOW);
-    if(kalmte < 5){
-    kalmte = kalmte + 1;
-    };
-}
-else if (kalmte > -3) {
+    if (kalmte < 5) {
+      kalmte = kalmte + 1;
+    }
+  }
+  else if (kalmte > -3) {
     kalmte = kalmte - 1;
-};
+  }
 
-if (kalmte > 0) {
+  if (kalmte > 0) {
     digitalWrite(2, HIGH);
     digitalWrite(3, LOW);
     digitalWrite(4, LOW);
-};
+  }
 
-/* // Debuging (optional)
-Serial.println("Analog:");
-Serial.println(analogValue);
-Serial.println("Overlast:");
-Serial.println(overlast);
-Serial.println("Kalmte:");
-Serial.println(kalmte);
-// End Debug
-*/
-delay(50);
+  delay(50);
 }
